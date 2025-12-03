0b2188e67b90aec2cb7d3bbe37d2b47848078674
romanornr <6548898+romanornr@users.noreply.github.com>
2025-12-03 02:03:39 +0100

    Bridge components barrel file for imports

---
+++ src/components/bridge/index.ts
@@ -0,0 +1,15 @@
+// src/components/bridge/index.ts
+/**
+ * Bridge Components
+ *
+ * Import from this barrel file for clean imports
+ */
+
+export { BridgeModeTabs, type BridgeMode } from "./bridge-mode-tabs";
+export { NetworkLaneSelector } from "./network-lane-selector";
+export { TransferAmountInput } from "./transfer-amount-input";
+export { AvailableBalanceDisplay } from "./available-balance-display";
+export { AmountSlider } from "./amount-slider";
+export { TransactionSummaryCard } from "./transaction-summary-card";
+export { RecipientAddressSection } from "./recipient-address-section";
+export { BridgeSubmitButton } from "./bridge-submit-button";
